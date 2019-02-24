a5706c525c87464facc32fba4db2d5f1d01f768b
David <david@linuxsoftware.co.nz>
2019-02-24 15:42:11 +1300

    Add missing event_overrides.html and names.py

---
+++ ls/joyous/utils/names.py
@@ -0,0 +1,35 @@
+# ------------------------------------------------------------------------------
+# Names for i18n
+# ------------------------------------------------------------------------------
+import sys
+from django.utils.translation import gettext_lazy as _
+from django.utils import dates
+
+# ------------------------------------------------------------------------------
+class _Names(tuple):
+    def __getitem__(self, i):
+        return str(super().__getitem__(i))
+
+# ------------------------------------------------------------------------------
+MONDAY_TO_SUNDAY   = _Names(dates.WEEKDAYS[k] for k in range(7))
+SUNDAY_TO_SATURDAY = _Names(dates.WEEKDAYS[k%7] for k in range(6,13))
+MON_TO_SUN = _Names(dates.WEEKDAYS_ABBR[k] for k in range(7))
+SUN_TO_SAT = _Names(dates.WEEKDAYS_ABBR[k%7] for k in range(6,13))
+
+# ------------------------------------------------------------------------------
+WEEKDAY_NAMES_PLURAL = _Names((_("Mondays"),
+                               _("Tuesdays"),
+                               _("Wednesdays"),
+                               _("Thursdays"),
+                               _("Fridays"),
+                               _("Saturdays"),
+                               _("Sundays")))
+
+# ------------------------------------------------------------------------------
+MONTH_NAMES = _Names(dates.MONTHS.get(k,"") for k in range(13))
+WRAPPED_MONTH_NAMES = _Names(dates.MONTHS[k%12+1] for k in range(11,25))
+MONTH_ABBRS = _Names(dates.MONTHS_3.get(k, "") for k in range(13))
+
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
