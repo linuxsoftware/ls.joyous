4d51b2c75b3b2aebdb08bc005814051eb1b23863
David <david@linuxsoftware.co.nz>
2021-09-21 00:03:47 +1200

    Unittests working with Wagtail 2.14

--- ls/joyous/models/event_base.py
+++ ls/joyous/models/event_base.py
@@ -13,8 +13,8 @@ from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 from django.utils.translation import gettext
 from timezone_field import TimeZoneField
-from wagtail.core.query import PageQuerySet
 from wagtail.core.models import Page, PageManager, PageViewRestriction
+from wagtail.core.query import PageQuerySet
 from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import (FieldPanel,
         PageChooserPanel, BaseCompositeEditHandler)
